85f48b52b04a63e1604041adb8bb1afb82af26bd
psatomas <psatomas@gmail.com>
2025-11-11 10:34:06 -0300

    feat(products): add validation for negative price and stock; update tests

--- testing_project/products/models.py
+++ testing_project/products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 class User(AbstractUser):
