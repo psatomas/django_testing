3f33b8d02d72f30883d2f0c957954b1c5492dff2
psatomas <psatomas@gmail.com>
2025-11-19 19:39:48 -0300

    feat: add products view with product list context

--- testing_project/products/views.py
+++ testing_project/products/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from products.models import Product
 # Create your views here.
 def homepage(request):
     return render(request, 'index.html')
+
+def products(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products.html', context)
