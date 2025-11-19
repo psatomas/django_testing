a8d785f26f8473d541abd3c1625035f65922b05c
psatomas <psatomas@gmail.com>
2025-11-19 18:29:49 -0300

    refactor(tests): remove redundant status code test and consolidate into template/content checks

--- testing_project/products/tests/test_views.py
+++ testing_project/products/tests/test_views.py
@@ -2,14 +2,10 @@ from django.test import TestCase, SimpleTestCase
 
 class TestHomePage(SimpleTestCase):
 
-    def test_homepage_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
     def test_homepage_uses_correct_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
 
     def test_homepage_contains_Welcome_message(self):
         response = self.client.get('/')
-        self.assertContains(response, 'Welcome to Our Store!')
+        self.assertContains(response, 'Welcome to Our Store!', status_code=200)
