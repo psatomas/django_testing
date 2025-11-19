d99281df42fcc05b701e546d1177b8b20cf9dfdf
psatomas <psatomas@gmail.com>
2025-11-19 18:06:25 -0300

    test: add assertion to check Welcome message on homepage

--- testing_project/products/tests/test_views.py
+++ testing_project/products/tests/test_views.py
@@ -9,3 +9,7 @@ class TestHomePage(SimpleTestCase):
     def test_homepage_uses_correct_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_homepage_contains_Welcome_message(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Welcome to Our Store!')
