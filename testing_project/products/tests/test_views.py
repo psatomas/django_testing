9898e7b39db701bdd239429c132dd2c4a254007c
psatomas <psatomas@gmail.com>
2025-11-19 20:09:26 -0300

    test: add setup with sample Product instances for products page tests

--- testing_project/products/tests/test_views.py
+++ testing_project/products/tests/test_views.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, SimpleTestCase
+from products.models import Product
+from django.urls import reverse
 
 class TestHomePage(SimpleTestCase):
 
@@ -9,3 +11,12 @@ class TestHomePage(SimpleTestCase):
     def test_homepage_contains_Welcome_message(self):
         response = self.client.get('/')
         self.assertContains(response, 'Welcome to Our Store!', status_code=200)
+
+class TestProductsPage(TestCase):
+
+    def setUp(self):
+        Product.objects.create(name="Laptop", price=1000)
+        Product.objects.create(name="Laptop", price=800)
+
+
+
