0c4162a44fa13cadaff3705390a56b386f18d74c
psatomas <psatomas@gmail.com>
2025-11-20 17:29:07 -0300

    feat: add stock_count validation and implement ProductForm handling in products view

--- testing_project/products/forms.py
+++ testing_project/products/forms.py
@@ -12,3 +12,10 @@ class ProductForm(forms.ModelForm):
         if price < 0:
             raise forms.ValidationError('price cannot be negative')
         return price
+    
+    def clean_stock_count(self):
+        """ Field level validation for the price field"""
+        price = self.cleaned_data.get('stock_count')
+        if price < 0:
+            raise forms.ValidationError('Stock count cannot be negative')
+        return price
