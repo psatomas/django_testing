10eec4f8ff5dc8d6eaf55b79eef0566f51b30e4d
psatomas <psatomas@gmail.com>
2025-11-20 17:10:17 -0300

    feat: add ProductForm with price validation

---
+++ testing_project/products/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from products.models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'price', 'stock_count')
+
+    def clean_price(self):
+        """ Field level validation for the price field"""
+        price = self.cleaned_data.get('price')
+        if price < 0:
+            raise forms.ValidationError('price cannot be negative')
+        return price
