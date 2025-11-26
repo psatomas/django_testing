b139e7886eabf77f8a5e78e7863762c756a3b63f
psatomas <psatomas@gmail.com>
2025-11-26 17:14:14 -0300

    test: add test ensuring site returns 503 when maintenance mode is on

--- testing_project/products/tests/test_middleware.py
+++ testing_project/products/tests/test_middleware.py
@@ -1,9 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 
 class MaintenanceModeTests(TestCase):
+
+    @override_settings(MAINTENANCE_MODE=False)
     def test_maintenance_mode_off(self):
         """Test that the site is accessible when maintenance mode is off."""
         response = self.client.get(reverse('home'))
-        self.assertContains(response, 'Welcome to Our Store!', status_code=200)
+
+        self.assertContains(response, "Welcome to Our Store!", status_code=200)
+
+    @override_settings(MAINTENANCE_MODE=True)
+    def test_maintenance_mode_on(self):
+        """Test that the site is accessible when maintenance mode is off."""
+        response = self.client.get(reverse('home'))
+
+        self.assertContains(response, "Site is under maintenance", status_code=503)
